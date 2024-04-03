6f870e7b38dc9c18b78dfda804339bffb60c354b
takumi3110 <takumi3110@garitto.jp>
2024-04-04 07:58:55 +0900

    update: BottomNavigationItem追加 - まとめて追加 - ゴミ箱 - 設定

--- lib/utils/widget_utils.dart
+++ lib/utils/widget_utils.dart
@@ -1,24 +1,5 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class WidgetUtils {
 
-  static Column FloatingAction() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Container(
-          margin: EdgeInsets.only(bottom: 70),
-          child: FloatingActionButton(
-            onPressed: () {
-              // Navigator.of(context).push(CupertinoPageRoute(builder: (context) => ))
-            },
-            child: Icon(CupertinoIcons.plus),
-            backgroundColor: Colors.green,
-          ),
-        )
-      ],
-      );
-}
 
 }
--- lib/view/screen.dart
+++ lib/view/screen.dart
@@ -15,19 +15,25 @@ class _ScreenState extends State<Screen> with WidgetsBindingObserver {
   int selectedIndex = 0;
   final _controller = CupertinoTabController();
   static List<Widget> pageList = [
-    const CalendarPage(),
     const ListPage(),
-
+    const CalendarPage(),
+    const Center(child: Text('まとめて追加')),
+    const Center(child: Text('ゴミ箱')),
+    const Center(child: Text('設定')),
   ];
 
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
+        height: 70,
         activeColor: Colors.green,
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.calendar), label: 'カレンダー'),
           BottomNavigationBarItem(icon: Icon(CupertinoIcons.list_bullet), label: 'リスト'),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.calendar), label: 'カレンダー'),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.plus_rectangle_fill_on_rectangle_fill), label: 'まとめて追加'),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.trash_fill), label: 'ゴミ箱'),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.gear_alt_fill), label: '設定'),
         ],
       ),
       tabBuilder: (BuildContext context, int index) {
